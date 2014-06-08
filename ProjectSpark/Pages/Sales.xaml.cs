3ebad9205560507c39e6200bdfaf625104af89a0
Thomas <thomas.reyskens@gmail.com>
2014-06-08 19:15:58 +0200

    Tabs 2
    Tabs aangepast met groter klikbaar gedeelte

--- ProjectSpark/Pages/Sales.xaml.cs
+++ ProjectSpark/Pages/Sales.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -28,7 +29,9 @@ namespace ProjectSpark.Pages
             scanner = new Scanner();
             scanner.scanEvent += scanner_scanEvent;
 
-            for (int i = 0; i < 15; i++)
+            
+
+            for (int i = 0; i < 27; i++)
             {
                 TabItem test = new TabItem();
                 test.Header = i.ToString() + " LOL";
