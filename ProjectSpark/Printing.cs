95105376cfbec809f133a9b34f5b872775ba89a8
Xehinz <tim.cool@gmail.com>
2014-06-26 09:55:05 +0200

    Added Printer Class

---
+++ ProjectSpark/Printing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace ProjectSpark
+{
+    class Printing
+    {
+        public static void Print(FlowDocument document)
+        {
+            // Create a PrintDialog
+            PrintDialog printDlg = new PrintDialog();
+
+            // Create a FlowDocument dynamically.
+            FlowDocument doc = document;
+            doc.Name = "FlowDoc";
+
+            // Create IDocumentPaginatorSource from FlowDocument
+            IDocumentPaginatorSource idpSource = doc;
+
+            // Call PrintDocument method to send document to printer
+            printDlg.PrintDocument(idpSource.DocumentPaginator, "Receipt");
+        }
+    }
+}
